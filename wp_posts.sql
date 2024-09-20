0b9ca20b82dc789ae9d576ee229d4d45c34d6f1a
TranNgocHien253 <tranngockara@gmail.com>
2024-09-20 16:37:11 +0700

    update sql

--- wp_posts.sql
+++ wp_posts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2024 at 11:20 AM
+-- Generation Time: Sep 20, 2024 at 11:36 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
